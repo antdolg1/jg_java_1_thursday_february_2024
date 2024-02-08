ff881ce6d5830768c2176882e23940789e5909d4
KRuSS24 <russ0273@gmail.com>
2024-02-08 22:51:00 +0200

    Ruslan Test (new line)

---
+++ src/main/java/students/ruslan_k/lesson_2/IntArithmetic.java
@@ -0,0 +1,21 @@
+package students.ruslan_k.lesson_2;
+
+import java.util.Scanner;
+
+public class IntArithmetic {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please input 1st INT: ");
+        int a = scan.nextInt();
+        System.out.print("Please input 2nd INT: ");
+        int b = scan.nextInt();
+        int c=a+b;
+        System.out.println("Result of " + a + "+" + b + " is " + c);
+        c=a-b;
+        System.out.println("Result of " + a + "-" + b + " is " + c);
+
+        System.out.println("Result of " + a + "*" + b + " is " + a*b);
+        System.out.println("Result of " + a + "/" + b + " is " + a/b);
+
+    }
+}
