7ad45cd71db7f1c30fc91375f18c077b5f067be0
KRuSS24 <russ0273@gmail.com>
2024-02-08 22:15:06 +0200

    Ruslan Test (new line)

--- src/main/java/students/ruslan_k/testArifmetic.java
+++ src/main/java/students/ruslan_k/testArifmetic.java
@@ -2,8 +2,8 @@ package students.ruslan_k;
 
 public class testArifmetic {
     public static void main(String[] args) {
-        int a= 10;
-        int b=4;
+        int a = 10;
+        int b = 4;
         double result = a / b;
 
         System.out.println(result);
