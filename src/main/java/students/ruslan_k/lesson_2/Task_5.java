a4102bb47e0c4719cc4b70045f3f337757edb042
KRuSS24 <russ0273@gmail.com>
2024-02-09 20:29:17 +0200

    Ruslan Lesson 2

---
+++ src/main/java/students/ruslan_k/lesson_2/Task_5.java
@@ -0,0 +1,7 @@
+package students.ruslan_k.lesson_2;
+
+class BankAccount {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/students/ruslan_k/lesson_2/Task_7.java
@@ -0,0 +1,8 @@
+package students.ruslan_k.lesson_2;
+
+class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+
+    }
+}
