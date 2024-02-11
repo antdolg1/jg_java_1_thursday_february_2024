6acda7c564d89201194363e667d020d5e68bf993
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2024-02-11 17:22:07 +0200

    lesson_2_level2

---
+++ src/main/java/students/slava_subacius/lesson_2_2/Phone.java
@@ -0,0 +1,7 @@
+package students.slava_subacius.lesson_2_2;
+
+public class Phone {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/students/slava_subacius/lesson_2_2/Task_8.java
@@ -0,0 +1,12 @@
+package students.slava_subacius.lesson_2_2;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
