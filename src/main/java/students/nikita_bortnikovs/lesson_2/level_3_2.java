d94d0c04558e47aaf328f57324ad05afa27177b3
nbortnikovs <n.bortnikovs@bill.me>
2024-02-12 15:53:05 +0200

    committing some more homework

---
+++ src/main/java/students/nikita_bortnikovs/lesson_2/level_3_2.java
@@ -0,0 +1,19 @@
+package students.nikita_bortnikovs.lesson_2;
+
+import java.util.Scanner;
+
+public class level_3_2 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi! Please enter here your radius to see the perimeter and area of the circle...");
+        double rad = scanner.nextDouble();
+        double per = 2*3.14*rad;
+        double ar = 3.14*Math.pow(rad, 2);
+
+        System.out.println("Your perimeter is  " + per+" cm");
+        System.out.println("Your area is  " + ar+" cm");
+
+
+    }
+}
---
+++ src/main/java/students/nikita_bortnikovs/lesson_2/level_4_1.java
@@ -0,0 +1,11 @@
+package students.nikita_bortnikovs.lesson_2;
+
+public class level_4_1 {
+    public static  void main(String[] args){
+        System.out.println("My JAVA  version is");
+        System.out.println("java version \"11.0.18\" 2023-01-17 LTS\n" +
+                "Java(TM) SE Runtime Environment 18.9 (build 11.0.18+9-LTS-195)\n" +
+                "Java HotSpot(TM) 64-Bit Server VM 18.9 (build 11.0.18+9-LTS-195, mixed mode)\n" +
+                "PS C:\\Users\\User> ");
+    }
+}
