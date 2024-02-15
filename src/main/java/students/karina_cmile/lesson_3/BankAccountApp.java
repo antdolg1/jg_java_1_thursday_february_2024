55afde9f74127d1a093df9d4770731478a110efb
KarinaSoha <karina.soha@gmail.com>
2024-02-15 19:39:51 +0200

    Lesson 3, Task2

--- src/main/java/students/karina_cmile/lesson_3/BankAccountApp.java
+++ src/main/java/students/karina_cmile/lesson_3/BankAccountApp.java
@@ -4,7 +4,6 @@ public class BankAccountApp {
     public static void main(String[] args) {
 
         BankAccount bankAccount1 = new BankAccount();
-
         BankAccount bankAccount2 = new BankAccount();
         BankAccount bankAccount3 = new BankAccount();
 
