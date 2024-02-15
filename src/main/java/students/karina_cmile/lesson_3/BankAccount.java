c94ad579218dbead02144b99b7dc784762d723fb
KarinaSoha <karina.soha@gmail.com>
2024-02-15 20:31:37 +0200

    Lesson 3, Task5

--- src/main/java/students/karina_cmile/lesson_3/BankAccount.java
+++ src/main/java/students/karina_cmile/lesson_3/BankAccount.java
@@ -2,12 +2,15 @@ package students.karina_cmile.lesson_3;
 
 public class BankAccount {
     String owner;
-
     int amount;
 
-    BankAccount(String owner) {
+    BankAccount(String owner, int amount) {
         this.owner = owner;
-}
+        this. amount = amount;
+    }
 
+    String getOwner() {
+        return this.owner;
+    }
 
 }
