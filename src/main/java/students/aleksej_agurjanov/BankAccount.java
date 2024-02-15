14c3fdd998cb8c48176c926c9fdf12963b017da1
aleksejagurjanov <xirou89@gmail.com>
2024-02-15 20:33:47 +0200

    Commit file

--- src/main/java/students/aleksej_agurjanov/BankAccount.java
+++ src/main/java/students/aleksej_agurjanov/BankAccount.java
@@ -1,7 +1,17 @@
 package students.aleksej_agurjanov;
 
 public class BankAccount {
-    String ower;
+    String owner;
 
     int amount;
+
+
+    BankAccount(String owner, int amount) {
+        this.owner = owner;
+        this.amount = amount;
+    }
+
+    String getOwner() {
+        return this.owner;
+    }
 }
