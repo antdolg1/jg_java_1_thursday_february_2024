38a7bd83704601eaad359f2aee490ca88bdb1e9c
KarinaSoha <karina.soha@gmail.com>
2024-02-15 19:39:21 +0200

    Lesson 3, Task2

---
+++ src/main/java/students/karina_cmile/lesson_3/BankAccountApp.java
@@ -0,0 +1,13 @@
+package students.karina_cmile.lesson_3;
+
+public class BankAccountApp {
+    public static void main(String[] args) {
+
+        BankAccount bankAccount1 = new BankAccount();
+
+        BankAccount bankAccount2 = new BankAccount();
+        BankAccount bankAccount3 = new BankAccount();
+
+
+    }
+}
