f72f925703fb70ce8f0e1cf528baccbacdd220e7
KRuSS24 <russ0273@gmail.com>
2024-02-15 20:36:58 +0200

    Ruslan

--- src/main/java/students/ruslan_k/lesson_3/BankAccountApp.java
+++ src/main/java/students/ruslan_k/lesson_3/BankAccountApp.java
@@ -3,8 +3,8 @@ package students.ruslan_k.lesson_3;
 public class BankAccountApp {
     public static void main(String[] args) {
         BankAccount acc1 = new BankAccount("Vovochka", 1000);
-        BankAccount acc2 = new BankAccount("Olen'ka", 5000);
-        BankAccount acc3 = new BankAccount("MarIvanna", 10000);
+        //BankAccount acc2 = new BankAccount("Olen'ka", 5000);
+        //BankAccount acc3 = new BankAccount("MarIvanna", 10000);
 
         System.out.println(acc1.getAmount() +"  " + acc1.getOwner());
         acc1.changeAmount(-100);
