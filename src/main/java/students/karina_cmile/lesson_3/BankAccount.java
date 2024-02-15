dc68f0ceae2a73bcb1ae47562cc1681f6469ca1a
KarinaSoha <karina.soha@gmail.com>
2024-02-15 20:01:10 +0200

    Lesson 3, Task3

--- src/main/java/students/karina_cmile/lesson_3/BankAccount.java
+++ src/main/java/students/karina_cmile/lesson_3/BankAccount.java
@@ -3,10 +3,11 @@ package students.karina_cmile.lesson_3;
 public class BankAccount {
     String owner;
 
-
     int amount;
 
-
+    BankAccount(String owner) {
+        this.owner = owner;
+}
 
 
 }
--- src/main/java/students/karina_cmile/lesson_3/BankAccountApp.java
+++ src/main/java/students/karina_cmile/lesson_3/BankAccountApp.java
@@ -3,10 +3,11 @@ package students.karina_cmile.lesson_3;
 public class BankAccountApp {
     public static void main(String[] args) {
 
-        BankAccount bankAccount1 = new BankAccount();
-        BankAccount bankAccount2 = new BankAccount();
-        BankAccount bankAccount3 = new BankAccount();
+        BankAccount bankAccount1 = new BankAccount(owner: "Vasja Pupkin");
+        BankAccount bankAccount2 = new BankAccount(owner: "Petja V");
+        BankAccount bankAccount3 = new BankAccount(owner: "Inga G");
 
+        BankAccount bankAccount4 = new BankAccount(owner: "Tatjana");
 
     }
 }
