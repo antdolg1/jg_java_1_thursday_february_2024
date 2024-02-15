2811754e691283680874ddc69e6f36c87929952b
KarinaSoha <karina.soha@gmail.com>
2024-02-15 21:16:36 +0200

    Lesson 3, Task7

---
+++ src/main/java/students/karina_cmile/lesson_3/lessoncode/Apple.java
@@ -0,0 +1,17 @@
+package students.karina_cmile.lesson_3.lessoncode;
+
+public class Apple {
+   private String sort;
+    private String color;
+    private int weight;
+
+    Apple(String sort, String color, int weight) {
+        this.sort = sort;
+        this.color = color;
+        this.weight = weight;
+    }
+    String getSort() {
+
+    } ;
+    String
+}
