a44d803473f59b4f81318f62c2d7c7583b192b96
javagurulv <javagurulv@gmail.com>
2024-02-15 19:40:46 +0200

    lesson 3 - BankAccount

--- src/main/java/teacher/lesson_3_oop_first_look/lessoncode/solutions/BankAccountApp.java
+++ src/main/java/teacher/lesson_3_oop_first_look/lessoncode/solutions/BankAccountApp.java
@@ -5,7 +5,6 @@ public class BankAccountApp {
     public static void main(String[] args) {
 
         BankAccount bankAccount1 = new BankAccount();
-
         BankAccount bankAccount2 = new BankAccount();
         BankAccount bankAccount3 = new BankAccount();
 
