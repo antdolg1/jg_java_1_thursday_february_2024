0125b9f249b57f704b22f3400c220741127800ad
aleksejagurjanov <xirou89@gmail.com>
2024-02-17 08:41:28 +0200

    Commit update text file

--- src/main/java/students/aleksej_agurjanov/Apple.java
+++ src/main/java/students/aleksej_agurjanov/Apple.java
@@ -2,11 +2,26 @@ package students.aleksej_agurjanov;
 
 public class Apple {
 
-    private String sort;
+    private String type;
     private String color;
     private int weight;
 
-    Apple(String sort, String color, String weight) {
-
+    public Apple(String type, String color, int weight) {
+        this.type = type;
+        this.color = color;
+        this.weight = weight;
     }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
 }
--- src/main/java/students/aleksej_agurjanov/AppleApp.java
+++ src/main/java/students/aleksej_agurjanov/AppleApp.java
@@ -1,8 +1,21 @@
 package students.aleksej_agurjanov;
 
+import teacher.lesson_3_oop_first_look.lessoncode.;
+
 public class AppleApp {
 
     public static void main(String[] args) {
-        Apple
+        Apple apple1 = new Apple("Antonovka", "Red", 100);
+        Apple apple2 = new Apple("Champion", "Yellow", 50);
+
+        System.out.println("Type = " + apple1.getType());
+        System.out.println("Type = " + apple2.getType());
+
+        System.out.println("Color = " + apple1.getColor());
+        System.out.println("Color = " + apple2.getColor());
+
+        System.out.println("Weight = " + apple1.getWeight());
+        System.out.println("Weight = " + apple2.getWeight());
+
     }
 }
