725ecac494f12bf1888c027b13a1276fe879027c
Aleksandra-Hodjuka <alexandra11@inbox.lv>
2024-02-18 19:39:13 +0200

    Updated text

---
+++ src/main/java/students/aleksandra_hodjuka/lesson_2/level_3/Task_9.java
@@ -0,0 +1,43 @@
+package students.aleksandra_hodjuka.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class Task_9 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter your number!");
+        int A = scanner.nextInt();
+
+
+        int A1 = A * 1;
+        int A2 = A * 2;
+        int A3 = A * 3;
+        int A4 = A * 4;
+        int A5 = A * 5;
+        int A6 = A * 6;
+        int A7 = A * 7;
+        int A8 = A * 8;
+        int A9 = A * 9;
+        int A10 = A * 10;
+
+        System.out.println(STR."\{A} *1 =\{A1}");
+        System.out.println(STR."\{A} *2 =\{A2}");
+        System.out.println(STR."\{A} *3 =\{A3}");
+        System.out.println(STR."\{A} *4 =\{A4}");
+        System.out.println(STR."\{A} *5 =\{A5}");
+        System.out.println(STR."\{A} *6 =\{A6}");
+        System.out.println(STR."\{A} *7 =\{A7}");
+        System.out.println(STR."\{A} *8 =\{A8}");
+        System.out.println(STR."\{A} *9 =\{A9}");
+        System.out.println(STR."\{A} *10 =\{A10}");
+
+
+
+
+    }
+
+
+}
