06babb602a9175aae483776f3c9eb17f79bebf61
Aleksandra-Hodjuka <alexandra11@inbox.lv>
2024-02-18 20:21:06 +0200

    Updated text

---
+++ src/main/java/students/aleksandra_hodjuka/lesson_2/level_5/Task_15.java
@@ -0,0 +1,18 @@
+package students.aleksandra_hodjuka.lesson_2.level_5;
+
+public class Task_15 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Напишите программу, которая демонстрирует использование команды!");
+
+
+
+
+    }
+
+
+
+
+
+}
