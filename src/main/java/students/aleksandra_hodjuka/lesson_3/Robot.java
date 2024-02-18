d9537edca64bbcdff87b7bab72aa98969fd987cf
Aleksandra-Hodjuka <alexandra11@inbox.lv>
2024-02-18 21:20:47 +0200

    Updated text

---
+++ src/main/java/students/aleksandra_hodjuka/lesson_3/Robot.java
@@ -0,0 +1,21 @@
+package students.aleksandra_hodjuka.lesson_3;
+
+public class Robot {
+
+
+    String name;
+
+    public Robot(String name) {
+        this.name = name;
+
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
