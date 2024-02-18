88602c3794e17e98dd8f755bc560fd8923bd7ee9
Aleksandra-Hodjuka <alexandra11@inbox.lv>
2024-02-18 19:54:09 +0200

    Updated text

---
+++ src/main/java/students/aleksandra_hodjuka/lesson_2/level_3/Task_11.java
@@ -0,0 +1,25 @@
+package students.aleksandra_hodjuka.lesson_2.level_3;
+
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter your first number!");
+        double A = scanner.nextInt();
+
+        System.out.println("Please, enter your second number!");
+        double B = scanner.nextInt();
+
+        System.out.println("Please, enter your third number!");
+        double C = scanner.nextInt();
+
+        double averageNumber = A + B + C / 3;
+
+
+        System.out.println(averageNumber);
+
+
+    }
+}
