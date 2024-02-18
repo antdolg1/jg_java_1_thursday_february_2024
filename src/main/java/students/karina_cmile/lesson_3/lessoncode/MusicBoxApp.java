45ad001eee1a64bc83aee4b386a1f041c6ea888f
KarinaSoha <karina.soha@gmail.com>
2024-02-18 23:08:45 +0200

    Homework, lesson3

--- src/main/java/students/karina_cmile/lesson_3/lessoncode/MusicBoxApp.java
+++ src/main/java/students/karina_cmile/lesson_3/lessoncode/MusicBoxApp.java
@@ -5,8 +5,45 @@ public class MusicBoxApp {
         MusicBox musicBox = new MusicBox("S90");
 
         System.out.println("Is on = " + musicBox.isSwitchedOn());
+        System.out.println("Is off = " + musicBox.isSwitchedOff());
+        System.out.println("Volume = " + musicBox.getVolume());
+
+        musicBox.switchOn();
+
         System.out.println("Is on = " + musicBox.isSwitchedOn());
-        System.out.println("Is on = " + musicBox.isSwitchedOn());
+        System.out.println("Is off = " + musicBox.isSwitchedOff());
+
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+
+        System.out.println("Volume = " + musicBox.getVolume());
+
+        musicBox.switchOff();
+
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+
+        System.out.println("Volume = " + musicBox.getVolume());
+
+
+        musicBox.switchOn();
+
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+        musicBox.increaseVolume();
+
+        System.out.println("Volume = " + musicBox.getVolume());
 
     }
 }
