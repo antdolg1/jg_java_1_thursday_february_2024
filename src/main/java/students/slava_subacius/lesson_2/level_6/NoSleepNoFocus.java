c21ae06f9f32aa67cc11fc3c2975b80f5508a0ca
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2024-02-20 12:55:23 +0200

    lesson_2_levels

---
+++ src/main/java/students/slava_subacius/lesson_2/level_6/NoSleepNoFocus.java
@@ -0,0 +1,10 @@
+package students.slava_subacius.lesson_2.level_6;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
+
+
---
+++ src/main/java/students/slava_subacius/lesson_2/level_7/SumTwoNumbers.java
@@ -0,0 +1,21 @@
+package students.slava_subacius.lesson_2.level_7;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 20;
+
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
+
