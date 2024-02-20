52064ebf84cdbfc027f34c6fd3177594f5c0d008
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-02-20 13:13:28 +0200

    Updated text files

---
+++ src/main/java/students/aleksandra_hodjuka/lesson_3/Robot.java
@@ -0,0 +1,19 @@
+package students.aleksandra_hodjuka.lesson_3;
+
+public class Robot {
+
+    private String name;
+
+    public Robot (String name); {
+    this.name = name;
+
+    }
+
+
+
+    public String getName() {
+        return this.name;
+    }
+
+
+}
---
+++ src/main/java/students/aleksandra_hodjuka/lesson_3/level_1/RobotApp.java
@@ -0,0 +1,15 @@
+package students.aleksandra_hodjuka.lesson_3.level_1;
+
+import students.aleksandra_hodjuka.lesson_3.Robot;
+
+public class RobotApp {
+
+    public static void main(String[] args) {
+
+        Robot Robot1 = new Robot();
+        Robot Robot2 = new Robot()
+        Robot Robot3 = new Robot()
+
+
+    }
+}
