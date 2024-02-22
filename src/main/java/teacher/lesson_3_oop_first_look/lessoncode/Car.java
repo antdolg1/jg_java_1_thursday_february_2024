d4171ded32ee2182d1148657f6318ac10a44f7c8
Antons Dolgopolovs <antdolg@gmail.com>
2024-02-22 19:38:26 +0200

    Add wheel of fortune feature

---
+++ src/main/java/teacher/lesson_3_oop_first_look/lessoncode/Car.java
@@ -0,0 +1,14 @@
+package teacher.lesson_3_oop_first_look.lessoncode;
+
+public class Car {
+    String name;
+    String color;
+
+    public Car(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Car() {
+    }
+}
---
+++ src/main/java/teacher/lesson_3_oop_first_look/lessoncode/CarApp.java
@@ -0,0 +1,11 @@
+package teacher.lesson_3_oop_first_look.lessoncode;
+
+public class CarApp {
+    public static void main(String[] args) {
+
+        Car myCar = new Car("Audi", "Red");
+        Car myCarTwo = new Car();
+        //codasdasd
+
+    }
+}
