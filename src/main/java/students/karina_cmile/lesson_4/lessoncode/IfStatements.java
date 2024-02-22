aa1869c69b6eb53139def1c46fe598f68d9623a0
KarinaSoha <karina.soha@gmail.com>
2024-02-22 22:06:03 +0200

    lesson4

---
+++ src/main/java/students/karina_cmile/lesson_4/lessoncode/IfStatements.java
@@ -0,0 +1,18 @@
+package students.karina_cmile.lesson_4.lessoncode;
+
+public class IfStatements {
+    public static void main(String[] args) {
+        int number = 10;
+        if(number >0) {
+            System.out.println ("Cislo poloziteljnoje");
+        }
+
+        int numberTwo = -5;
+        if (numberTwo > 0) {System.out.println ("Cislo poloziteljnoje");}
+     else {System.out.println("Chislo otricateljnoje");}
+
+}
+}
+
+
+
