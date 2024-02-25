5c9ea2817c1a6d568a42f1cad5f7b38fa7160ed8
KarinaSoha <karina.soha@gmail.com>
2024-02-25 21:35:22 +0200

    lesson4

---
+++ src/main/java/students/karina_cmile/lesson_4/lessoncode/IfStatements.java
@@ -0,0 +1,46 @@
+package students.karina_cmile.lesson_4.lessoncode;
+
+public class IfStatements {
+    public static void main(String[] args) {
+
+
+        int number = 10;
+        if (number > 0) {
+            System.out.println("Число положительное");
+        }
+
+        int numberTwo = -5;
+        if (numberTwo > 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+
+        int examScore = 54;
+
+        if (examScore >= 90) {
+            System.out.println("Отлично!");
+        } else if (examScore >= 70) {
+            System.out.println("Хорошо");
+        } else if (examScore >= 50) {
+            System.out.println("Удовлетворительно");
+        } else if (examScore >=30) {
+            System.out.println("Неочень!");
+        } else {
+            System.out.println("ОТЧИСЛЕН!!!");
+        }
+
+        String fruit = "lemon";
+
+        if (fruit.equals("orange")) {
+            System.out.println("Orange color");
+        } else if (fruit.equals("lemon")) {
+            System.out.println("Yellow color");
+        } else if (fruit.equals("Apple")) {
+            System.out.println("Red color");
+        } else {
+            System.out.println("Unknown fruit");
+        }
+
+    }
+}
---
+++ src/main/java/students/karina_cmile/lesson_4/lhomework/level_1_intern/Task_1.java
@@ -0,0 +1,21 @@
+package students.karina_cmile.lesson_4.lhomework.level_1_intern;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("Please enter the integer number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is positive");
+        } else if (number == 0) {
+            System.out.println("Please enter the integer number.");
+        } else if (number < 0) {
+            System.out.println(number + " is negative");
+        }
+    }
+
+
+}
