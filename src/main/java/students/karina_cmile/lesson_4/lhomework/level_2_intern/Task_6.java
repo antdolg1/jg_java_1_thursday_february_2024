a8d386362dbeb8f82e20671c670df668eb1fa0a1
KarinaSoha <karina.soha@gmail.com>
2024-02-26 18:11:32 +0200

    lesson4 homework

---
+++ src/main/java/students/karina_cmile/lesson_4/lhomework/level_2_intern/Task_6.java
@@ -0,0 +1,21 @@
+package students.karina_cmile.lesson_4.lhomework.level_2_intern;
+
+import java.util.Scanner;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        System.out.println("Please enter number x: ");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        System.out.println("Please enter number y: ");
+        int number2 = scanner.nextInt();
+
+        if (number1 > number2) {
+            System.out.println(number2 + " is the smallest number");
+        } else if (number1 < number2) {
+            System.out.println(number1 + " is the smallest number");
+        } else {
+            System.out.println("Please enter the different numbers!");
+        }
+    }
+}
