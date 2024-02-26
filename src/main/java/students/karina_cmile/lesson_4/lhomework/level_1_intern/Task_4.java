b2462155fc82c836536b96e0af97276bf7fe39ad
KarinaSoha <karina.soha@gmail.com>
2024-02-26 17:45:30 +0200

    lesson4 homework

--- src/main/java/students/karina_cmile/lesson_4/lhomework/level_1_intern/Task_4.java
+++ src/main/java/students/karina_cmile/lesson_4/lhomework/level_1_intern/Task_4.java
@@ -8,5 +8,10 @@ public class Task_4 {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
+        if (number % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
     }
 }
