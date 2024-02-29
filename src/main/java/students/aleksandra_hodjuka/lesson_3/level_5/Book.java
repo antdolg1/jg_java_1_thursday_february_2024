b5e4c9478b38596c63692ab20ed227ff52c30d62
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-02-29 12:40:25 +0200

    Homework_lesson_3_level_4

---
+++ src/main/java/students/aleksandra_hodjuka/lesson_3/level_5/Book.java
@@ -0,0 +1,29 @@
+package students.aleksandra_hodjuka.lesson_3.level_5;
+
+ class Book {
+
+
+    private String title;
+
+    Book(String title) {
+        this.title = title;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String title = myBook.getTitle();
+        System.out.println("Book title = " + title);
+    }
+
+}
+
+
+
