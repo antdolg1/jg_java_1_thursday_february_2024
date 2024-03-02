3f7ab49a93cd6106e9f3ad74a054d71c90e17423
KarinaSoha <karina.soha@gmail.com>
2024-03-03 01:19:45 +0200

    lesson5 internal homework

---
+++ src/main/java/students/karina_cmile/lesson_5/homework/level_2_intern/Task_11.java
@@ -0,0 +1,25 @@
+package students.karina_cmile.lesson_5.homework.level_2_intern;
+
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        int number2;
+        int number3;
+        int[] numbers = new int[3];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number 1: ");
+        numbers[0] = scanner.nextInt();
+        System.out.println("Enter number 2: ");
+        numbers[1] = scanner.nextInt();
+        System.out.println("Enter number 3: ");
+        numbers[2] = scanner.nextInt();
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Value of cell " + i + " = " + numbers[i]);
+        }
+
+
+    }
+}
