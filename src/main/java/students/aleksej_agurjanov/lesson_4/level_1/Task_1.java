afe80ab0544002144fe1746ed0e51501a7d9ae56
aleksejagurjanov <xirou89@gmail.com>
2024-03-03 13:28:06 +0200

    Commit file lesson_4

---
+++ src/main/java/students/aleksej_agurjanov/lesson_4/level_1/Task_1.java
@@ -0,0 +1,18 @@
+package students.aleksej_agurjanov.lesson_4.level_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        int number = 20;
+        if (number > 0) {
+            System.out.println("Число положительное");
+        }
+
+        int numberTwo = -10;
+        if  (numberTwo > 0) {
+            System.out.println("Число положительное");
+        }else {
+            System.out.println("Число отрицательное");
+        }
+    }
+}
