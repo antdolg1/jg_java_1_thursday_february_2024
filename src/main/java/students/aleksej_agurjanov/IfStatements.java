a0c26b30c1460e634fd69c05adbdb87dd7a466ba
aleksejagurjanov <xirou89@gmail.com>
2024-03-03 09:49:36 +0200

    Commit text file

---
+++ src/main/java/students/aleksej_agurjanov/IfStatements.java
@@ -0,0 +1,11 @@
+package students.aleksej_agurjanov;
+
+public class IfStatements {
+    public static void main(String[] args) {
+
+
+        int number = 10;
+        if
+
+    }
+}
--- src/main/java/students/aleksej_agurjanov/lesson_2/level_1/level_2/Task_4.java
+++ src/main/java/students/aleksej_agurjanov/lesson_2/level_1/level_2/Task_4.java
@@ -2,6 +2,12 @@ package students.aleksej_agurjanov.lesson_2.level_1.level_2;
 
 class Phone{
 
+    private String Name;
+
+    private String Type;
+
+    private String Price;
+
 
 
 
