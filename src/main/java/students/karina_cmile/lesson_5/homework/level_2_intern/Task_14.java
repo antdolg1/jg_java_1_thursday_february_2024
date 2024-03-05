661a58b1d3700a7823a39ff78d7fa54212f2f3e9
KarinaSoha <karina.soha@gmail.com>
2024-03-05 23:38:47 +0200

    lesson 5, homework

--- src/main/java/students/karina_cmile/lesson_5/homework/level_2_intern/Task_14.java
+++ src/main/java/students/karina_cmile/lesson_5/homework/level_2_intern/Task_14.java
@@ -1,13 +1,10 @@
 package students.karina_cmile.lesson_5.homework.level_2_intern;
 
-import teacher.lesson_5.lessoncode.CustomArrayUtils;
-
-import java.sql.SQLOutput;
 import java.util.Random;
 
-public class Task_13 {
+public class Task_14 {
     public static void main(String[] args) {
-               int[] numbers = new int[3];
+        int[] numbers = new int[3];
         Random random = new Random();
 
         for (int i = 0; i < numbers.length; i++) {
@@ -15,7 +12,12 @@ public class Task_13 {
         }
 //        System.out.println(numbers[0]+" "+numbers[1]+" "+numbers[2]);
 
-            int sum = ArraySum.sumOfArrayNumbers(numbers);
-            System.out.println("Сумма всех чисел массива равна " + sum);
-        }
+        double sum = ArraySum.sumOfArrayNumbers(numbers);
+
+//        System.out.println("Сумма всех чисел массива равна " + sum);
+
+        double average = sum / 3;
+        System.out.println("Cреднее значение равно " + average);
+
     }
+}
