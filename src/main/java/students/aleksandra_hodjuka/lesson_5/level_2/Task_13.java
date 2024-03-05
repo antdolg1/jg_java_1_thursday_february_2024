db822d680f294ca4248cf604de8a39e130791c0e
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-03-05 16:21:59 +0200

    Homework_lesson_5_corrected_mistakes

--- src/main/java/students/aleksandra_hodjuka/lesson_5/level_2/Task_13.java
+++ src/main/java/students/aleksandra_hodjuka/lesson_5/level_2/Task_13.java
@@ -1,24 +1,25 @@
 package students.aleksandra_hodjuka.lesson_5.level_2;
 
-public class Task_15 {
+public class Task_13 {
     public static void main(String[] args) {
         int[] numbers = new int[3];
 
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) (Math.random() * 100);
+            numbers[i] = (int) (Math.random() * 55);
         }
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("[" + i + "]" + " = " + numbers[i]);
         }
 
+        int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] >= 0) numbers[i] += 2;
+            sum = sum + numbers[i];
         }
-        System.out.println("Увеличение значения элементов массива на 2");
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("[" + i + "]" + " = " + numbers[i]);
-        }
+        System.out.println("Сумма всех ячеек массива равна  " + sum);
+
     }
 }
+
+
--- src/main/java/students/aleksandra_hodjuka/lesson_5/level_2/Task_14.java
+++ src/main/java/students/aleksandra_hodjuka/lesson_5/level_2/Task_14.java
@@ -1,24 +1,22 @@
 package students.aleksandra_hodjuka.lesson_5.level_2;
 
-public class Task_15 {
+public class Task_14 {
     public static void main(String[] args) {
         int[] numbers = new int[3];
 
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (int) (Math.random() * 100);
+            numbers[i] = (int) (Math.random() * 55);
         }
-
+        int sum = 0;
+        int average = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+            average = sum / numbers.length;
+        }
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("[" + i + "]" + " = " + numbers[i]);
         }
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] >= 0) numbers[i] += 2;
-        }
-        System.out.println("Увеличение значения элементов массива на 2");
-
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("[" + i + "]" + " = " + numbers[i]);
-        }
+        System.out.println(" Cреднее значение  " + average);
     }
+
 }
