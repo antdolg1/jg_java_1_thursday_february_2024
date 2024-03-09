d1f8b3144ac55d29e1f981f8b2158592cd58838e
KarinaSoha <karina.soha@gmail.com>
2024-03-09 22:07:01 +0200

    lesson 6, homework

---
+++ src/main/java/students/karina_cmile/lesson_6/level_1_intern/Task_6.java
@@ -0,0 +1,13 @@
+package students.karina_cmile.lesson_6.level_1_intern;
+
+class NumberUtils {
+
+    public boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
