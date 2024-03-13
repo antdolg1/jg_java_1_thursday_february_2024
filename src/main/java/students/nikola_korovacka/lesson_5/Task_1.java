1f42c3b6a7a7ea48a6e73492f0735ea01273730b
nikola.korovacka <nikkorovacka@gmail.com>
2024-03-13 20:19:10 +0200

    Lesson 5 homework and 6

---
+++ src/main/java/students/nikola_korovacka/lesson_5/Task_1.java
@@ -0,0 +1,11 @@
+package students.nikola_korovacka.lesson_5;
+
+public class Task_1 {
+    class ArrayDefinition1 {
+
+        public static void main(String[] args) {
+            int[] numbers = new int[5]; //массив с размером 5
+        }
+
+    }
+}
---
+++ src/main/java/students/nikola_korovacka/lesson_5/Task_6.java
@@ -0,0 +1,9 @@
+package students.nikola_korovacka.lesson_5;
+
+public class Task_6 {
+    class ArrayDefinition6 {
+        public static void main(String[] args) {
+            int[] numbers = {1, 2, 3};
+        }
+    }
+}
