896708d5b1c46f559b6c47b97988708f83b296cd
n.bortnikovs <n.bortnikovs@bill.me>
2024-03-14 15:49:31 +0200

    some extra homework for 6th lesson

---
+++ src/main/java/students/nikita_bortnikovs/lesson6/homework/level4/ForInfiniteLoopBreak.java
@@ -0,0 +1,10 @@
+package students.nikita_bortnikovs.lesson6.homework.level4;
+
+public class ForInfiniteLoopBreak {
+    public static void main(String[] args) {
+        for(int x=0;x<2;x--){
+            System.out.println("only one loop");
+            break;
+        }
+    }
+}
