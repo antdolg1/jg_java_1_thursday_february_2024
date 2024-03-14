2d13f1838ec11bf93dc843cee9d2cfba8bad0b67
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-03-14 16:27:24 +0200

    Homework_lesson_4_level_4_homework_added

--- src/main/java/students/aleksandra_hodjuka/lesson_4/level_4/Calculator.java
+++ src/main/java/students/aleksandra_hodjuka/lesson_4/level_4/Calculator.java
@@ -1,10 +1,11 @@
 package students.aleksandra_hodjuka.lesson_4.level_4;
 
- class Calculator {
+class Calculator {
 
 
     int firstNumber;
     int secondNumber;
+    int thirdNumber;
 
 
     public int sum(int firstNumber, int secondNumber) {
@@ -14,20 +15,52 @@ package students.aleksandra_hodjuka.lesson_4.level_4;
     public int sub1(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
+
     public int sub2(int secondNumber, int firstNumber) {
         return secondNumber - firstNumber;
     }
+
     public int mul(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
     }
+
     public int div1(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
     }
+
     public int div2(int secondNumber, int firstNumber) {
         return secondNumber / firstNumber;
     }
 
+    public boolean isEven(int realResult) {
+        return realResult % 2 == 0;
+    }
 
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber) {
+        if (firstNumber > secondNumber) {
+            return firstNumber;
+        } else return secondNumber;
+    }
 
- }
+    public int equalNumbers(int firstNumber, int secondNumber) {
+        if (firstNumber == secondNumber) {
+            return firstNumber;
+        } else return secondNumber;
+    }
+    public int maxOfThreeNumbers(int firstNumber, int secondNumber,int thirdNumber) {
+        if (firstNumber > secondNumber) {
+            return firstNumber;
+        } else if (firstNumber > thirdNumber)
+            return firstNumber;
+        else if (secondNumber > thirdNumber)
+            return secondNumber;
+        else return thirdNumber;}
+
+    public int threeEqualNumbers(int firstNumber, int secondNumber, int thirdNumber) {
+        if (firstNumber == (secondNumber = thirdNumber)) {
+            return firstNumber;
+        } else return secondNumber;
+    }
+}
+
 
--- src/main/java/students/aleksandra_hodjuka/lesson_4/level_4/CalculatorTest.java
+++ src/main/java/students/aleksandra_hodjuka/lesson_4/level_4/CalculatorTest.java
@@ -10,8 +10,111 @@ public class CalculatorTest {
         calculatorTest.mulTest();
         calculatorTest.div1Test();
         calculatorTest.div2Test();
+        calculatorTest.firstNumberIsBigger();
+        calculatorTest.secondNumberIsBigger();
+        calculatorTest.numbersAreEqual();
+        calculatorTest.firstOfThreeNumberIsBigger();
+        calculatorTest.secondOfThreeNumberIsBigger();
+        calculatorTest.thirdOfThreeNumberIsBigger();
+        calculatorTest.allThreeNumbersAreEqual();
+
+    }
+
+    private void allThreeNumbersAreEqual() {
+        int firstNumber = 30;  // подготавливаем тестовые данные
+        int secondNumber = 30;
+        int thirdNumber = 30;// подготавливаем тестовые данные
+        Calculator calculator = new Calculator();
+        int realResult = calculator.threeEqualNumbers(firstNumber, secondNumber,thirdNumber);
+        if (realResult == firstNumber) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Все три  числа равны = OK");
+        } else {
+
+            System.out.println("Все три равны = FAIL");
+        }
+    }
+
+    private void thirdOfThreeNumberIsBigger() {
+        int firstNumber = 50;  // подготавливаем тестовые данные
+        int secondNumber = 70;  // подготавливаем тестовые данные
+        int thirdNumber = 100;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == thirdNumber) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Третье число больше = OK");
+        } else {
+
+            System.out.println("Третье число больше = FAIL");
+        }
+    }
+
+    private void secondOfThreeNumberIsBigger() {
+        int firstNumber = 20;  // подготавливаем тестовые данные
+        int secondNumber = 100;  // подготавливаем тестовые данные
+        int thirdNumber = 20;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+        if (realResult == secondNumber) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Второе число больше = OK");
+        } else {
+
+            System.out.println("Второе число больше = FAIL");
+        }
+    }
+
+    private void firstOfThreeNumberIsBigger() {
+            int firstNumber = 50;  // подготавливаем тестовые данные
+            int secondNumber = 30;  // подготавливаем тестовые данные
+            int thirdNumber = 20;
+        Calculator calculator = new Calculator();
+            int realResult = calculator.maxOfThreeNumbers(firstNumber, secondNumber, thirdNumber);
+            if (realResult == firstNumber) {  // проверяем правильно ли сработал тестируемый код
+                System.out.println("Первое число больше = OK");
+            } else {
+
+                System.out.println("Первое число больше = FAIL");
+            }
+    }
 
 
+
+    private void numbersAreEqual() {
+        int firstNumber = 30;  // подготавливаем тестовые данные
+        int secondNumber = 30;  // подготавливаем тестовые данные
+        Calculator calculator = new Calculator();
+        int realResult = calculator.equalNumbers(firstNumber, secondNumber);
+        if (realResult == firstNumber) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Оба числа равны = OK");
+        } else {
+
+            System.out.println("Оба числа равны = FAIL");
+        }
+    }
+
+    private void secondNumberIsBigger() {
+        int firstNumber = 20;  // подготавливаем тестовые данные
+        int secondNumber = 50;  // подготавливаем тестовые данные
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfTwoNumbers(firstNumber, secondNumber);
+        if (realResult == secondNumber) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Второе число больше = OK");
+        } else {
+
+            System.out.println("Второе число больше = FAIL");
+        }
+    }
+
+    private void firstNumberIsBigger() {
+        int firstNumber = 50;  // подготавливаем тестовые данные
+        int secondNumber = 30;  // подготавливаем тестовые данные
+        Calculator calculator = new Calculator();
+        int realResult = calculator.maxOfTwoNumbers(firstNumber, secondNumber);
+        if (realResult == firstNumber) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Первое число больше = OK");
+        } else {
+
+            System.out.println("Первое число больше = FAIL");
+        }
     }
 
     private void div2Test() {
@@ -21,12 +124,16 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         int realResult = calculator.div2(secondNumber, firstNumber);  // вызываем продакшен код, который хотим протестировать
         if (realResult == expectedResult) {  // проверяем правильно ли сработал тестируемый код
-            System.out.println("Div1 test = OK");
+            System.out.println("Div2 test = OK");
         } else {
 
-            System.out.println("Div1 test = FAIL");
+            System.out.println("Div2 test = FAIL");
         }
-
+        if (realResult % 2 == 0) {
+            System.out.println("Результат Div2 test четное число");
+        } else {
+            System.out.println("Результат Div2 test нечетное число");
+        }
     }
 
     private void div1Test() {
@@ -42,7 +149,11 @@ public class CalculatorTest {
 
             System.out.println("Div1 test = FAIL");
         }
-
+        if (realResult % 2 == 0) {
+            System.out.println("Результат 1 test четное число");
+        } else {
+            System.out.println("Результат Div1 test нечетное число");
+        }
     }
 
     private void mulTest() {
@@ -54,8 +165,12 @@ public class CalculatorTest {
         if (realResult == expectedResult) {  // проверяем правильно ли сработал тестируемый код
             System.out.println("Mul test = OK");
         } else {
-
             System.out.println("Mul test = FAIL");
+        }
+        if (realResult % 2 == 0) {
+            System.out.println("Результат MUL test четное число");
+        } else {
+            System.out.println("Результат MUL test нечетное число");
         }
     }
 
@@ -71,6 +186,11 @@ public class CalculatorTest {
 
             System.out.println("Sub2 test = FAIL");
         }
+        if (realResult % 2 == 0) {
+            System.out.println("Результат Sub2 test четное число");
+        } else {
+            System.out.println("Результат Sub2 test нечетное число");
+        }
     }
 
     private void sumTest() {
@@ -84,6 +204,11 @@ public class CalculatorTest {
         } else {
 
             System.out.println("Sum test = FAIL");
+        }
+        if (realResult % 2 == 0) {
+            System.out.println("Результат Sum test четное число");
+        } else {
+            System.out.println("Результат Sum test нечетное число");
         }
 
     }
@@ -100,7 +225,10 @@ public class CalculatorTest {
 
             System.out.println("Sub1 test = FAIL");
         }
-
-
+        if (realResult % 2 == 0) {
+            System.out.println("Результат Sub1 test четное число");
+        } else {
+            System.out.println("Результат Sub1 test нечетное число");
+        }
     }
 }
