cf553a60414eb979c4246ddba2806c8ad0a1080f
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2024-03-18 08:31:06 +0200

    lesson_6_level_1_исправления

--- src/main/java/students/slava_subacius/lesson_6/level_1/NumberUtils.java
+++ src/main/java/students/slava_subacius/lesson_6/level_1/NumberUtils.java
@@ -2,10 +2,6 @@ package students.slava_subacius.lesson_6.level_1;
 
 public class NumberUtils {
     public boolean isEven(int number) {
-        if (number % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return (number % 2 == 0);
     }
 }
--- src/main/java/students/slava_subacius/lesson_6/level_1/TestIsEven.java
+++ src/main/java/students/slava_subacius/lesson_6/level_1/TestIsEven.java
@@ -1,12 +1,26 @@
 package students.slava_subacius.lesson_6.level_1;
 
-import java.util.Random;
-
 public class TestIsEven {
     public static void main(String[] args) {
-        NumberUtils numberUtils = new NumberUtils();
-        Random random = new Random();
-        int number = random.nextInt(100);
-        System.out.println("Number: " + number + " is even: " + numberUtils.isEven(number));
+        testIsEvenForEvenNumber();
+        testIsEvenForOddNumber();
+        testIsEvenForZero();
+        testIsEvenForNegativNumber();
     }
+     static void testIsEvenForEvenNumber() {
+         NumberUtils numberUtils = new NumberUtils();
+         System.out.println("is 4 even? " + numberUtils.isEven(4));
+     }
+     static void testIsEvenForOddNumber(){
+        NumberUtils numberUtils = new NumberUtils();
+         System.out.println("Is 5 even? " + numberUtils.isEven(5));
+     }
+     static void testIsEvenForZero(){
+        NumberUtils numberUtils = new NumberUtils();
+         System.out.println("Is 0 even? " + numberUtils.isEven(0));
+     }
+     static void testIsEvenForNegativNumber(){
+        NumberUtils numberUtils = new NumberUtils();
+         System.out.println("Is -3 even? " + numberUtils.isEven(-3));
+     }
 }
