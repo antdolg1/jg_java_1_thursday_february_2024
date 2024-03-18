65a06deaa935320146c6dce04d6ffc32e5b15bc5
MANKRA <kristina.manevica@if.lv>
2024-03-18 22:06:06 +0200

    Homework for lesson 6

---
+++ src/main/java/students/kristina_manevica/lesson_6/homework/level_1/WhileDefinition.java
@@ -0,0 +1,12 @@
+package students.kristina_manevica.lesson_6.homework.level_1;
+
+class WhileDefinition {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
