90caa89e2b717269e3e6e7ff03682f664807e7a0
KarinaSoha <karina.soha@gmail.com>
2024-03-20 23:50:53 +0200

    lesson 7, homework

--- src/main/java/students/karina_cmile/lesson_7/level_1_intern/WordService.java
+++ src/main/java/students/karina_cmile/lesson_7/level_1_intern/WordService.java
@@ -2,12 +2,49 @@ package students.karina_cmile.lesson_7.level_1_intern;
 
 import java.util.*;
 
-class WordService {
-    public static void main(String[] args) {
+public class WordService {
 
+    public static String splitTheSentence() {
+        String sentence = "Word1, Word1, Word2, Word1";
+        String[] words = sentence.split("[, ?.@]+");
+        Arrays.sort(words);
+        System.out.println(Arrays.toString(words));
+        return sentence;
+    }
+
+    public static String findTheMostFrequentWord(String[] words) {
+        int max = 0;
+        int count = 1;
+        String word = words[0];
+        String curr = words[0];
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].equals(curr)) {
+                count++;
+            } else {
+                count = 1;
+                curr = words[i];
+            }
+            if (max < count) {
+                max = count;
+                word = words[i];
+            }
+        }
+        System.out.println("The most frequent word in the sentence is "+ word+ " and it is repeated "+max + " times.");
+        return word;
+    }
+
+    public static void main(String[] args) {
+        String wordArr = WordService.splitTheSentence();
+        // WordService.findTheMostFrequentWord();
+
+    }
+
+}
+
+
+/*
         String sentence = "Word1, Word1, Word2, Word1, Word3, Word3, Word3, Word3, Word3, Word1, Word1, Word1";
         String[] words = sentence.split("[, ?.@]+"); // Splitting text into words and making an array.
-
         Arrays.sort(words);
         System.out.println(Arrays.toString(words)); // Output the array.
         int max = 0; // Finding the frequency of words
@@ -29,5 +66,8 @@ class WordService {
         }
         System.out.println("The most frequent word in the sentence is "+ word+ " and it is repeated "+max + " times.");
     }
-}
+ */
+
+
+
 
