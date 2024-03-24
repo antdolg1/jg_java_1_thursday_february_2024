8868f02e9a23b382dbb700fe81c773ad0fe07eed
KarinaSoha <karina.soha@gmail.com>
2024-03-24 20:42:53 +0200

    lesson 7, homework

---
+++ src/test/java/students/karina_cmile/lesson_7/level_1_intern/WordServiceTest.java
@@ -0,0 +1,19 @@
+package students.karina_cmile.lesson_7.level_1_intern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void splitTheSentence() {
+        // Тестирование с текстом, в котором все слова встречаются один раз
+    }
+
+    @org.junit.jupiter.api.Test
+    void findTheMostFrequentWord() {
+    }
+
+//    @org.junit.jupiter.api.Test
+//    void main() {
+//    }
+}
