674e323779b7e402718ffcc98182d28708235e2d
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-03-25 16:21:46 +0200

    Homework_lesson_4_level_5_homework_added

--- src/main/java/students/aleksandra_hodjuka/lesson_5/level_2/Task_15App.java
+++ src/main/java/students/aleksandra_hodjuka/lesson_5/level_2/Task_15App.java
@@ -11,17 +11,16 @@ public class Task_15App {
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("[" + i + "]" + " = " + numbers[i]);
         }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= 0) numbers[i] += 2;
+        }
 
-        for (int i = 0; i < numbers.length; i++)
-            if (numbers[i] >= 0) numbers[i] += 1;
 
-        {
-            int increase = Task_15.increasedNumbers(numbers);
-            System.out.println("Увеличение значения элементов массива на 2" );
+        System.out.println("Увеличение значения элементов массива на 2");
 
-            for (int i = 0; i < numbers.length; i++) {
-                System.out.println("[" + i + "]" + " = " + numbers[i]);
-            }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("[" + i + "]" + " = " + numbers[i]);
         }
     }
 }
+
