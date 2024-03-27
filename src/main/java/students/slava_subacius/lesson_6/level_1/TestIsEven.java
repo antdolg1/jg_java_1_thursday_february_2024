008ebfb2d9e069c0d67dadf36c2f032676c7c105
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2024-03-27 15:02:38 +0200

    lesson_6_test

--- src/main/java/students/slava_subacius/lesson_6/level_1/TestIsEven.java
+++ src/main/java/students/slava_subacius/lesson_6/level_1/TestIsEven.java
@@ -7,22 +7,45 @@ public class TestIsEven {
         testIsEvenForZero();
         testIsEvenForNegativNumber();
     }
-     static void testIsEvenForEvenNumber() {
-         NumberUtils numberUtils = new NumberUtils();
-         System.out.println("is 4 even? " + numberUtils.isEven(4));
-     }
-     static void testIsEvenForOddNumber(){
-        NumberUtils numberUtils = new NumberUtils();
-         System.out.println("Is 5 even? " + numberUtils.isEven(5));
-     }
-     static void testIsEvenForZero(){
-        NumberUtils numberUtils = new NumberUtils();
-         System.out.println("Is 0 even? " + numberUtils.isEven(0));
-     }
-     static void testIsEvenForNegativNumber(){
-        NumberUtils numberUtils = new NumberUtils();
-         System.out.println("Is -3 even? " + numberUtils.isEven(-3));
-     }
 
+    static void testIsEvenForEvenNumber() {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean actualResult = numberUtils.isEven(4);
+        boolean expectedResult = true;
+        printTestResult(4, actualResult, expectedResult);
+    }
+
+    static void testIsEvenForOddNumber() {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean actualResult = numberUtils.isEven(5);
+        boolean expectedResult = false;
+        printTestResult(5, actualResult, expectedResult);
+    }
+
+    static void testIsEvenForZero() {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean actualResult = numberUtils.isEven(0);
+        boolean expectedResult = true;
+        printTestResult(0, actualResult, expectedResult);
+    }
+
+    static void testIsEvenForNegativNumber() {
+        NumberUtils numberUtils = new NumberUtils();
+        boolean actualResult = numberUtils.isEven(-3);
+        boolean expectedResult = false;
+        printTestResult(-3, actualResult, expectedResult);
+    }
+
+    static void printTestResult(int number, boolean actualResult, boolean expectedResult) {
+        if (actualResult == expectedResult) {
+            System.out.println("Test passed for: " + number);
+        } else {
+            System.out.println("Test failed for: " + number + ". Expected: " +
+                    expectedResult + " but was: " + actualResult);
+        }
+    }
 }
+
+
+
 
