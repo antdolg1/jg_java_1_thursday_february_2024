2aa486e52c7a341dc2208ae22dcf5e1121d9aa02
tnejq <tnejq2012@gmail.com>
2024-03-28 21:41:48 +0200

    Все исправил.

---
+++ src/main/java/students/arturs_novikovs/lesson_4/lesson/Lesson_4_1.java
@@ -0,0 +1,15 @@
+package students.arturs_novikovs.lesson_4.lesson;
+
+public class Lesson_4_1 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("i value is: " + i);
+i = i +1;
+        System.out.println("i value is: " + i);
+i +=1;
+        System.out.println("i value is: " + i);
+i++;
+        System.out.println("i value is: " + i);
+
+    }
+}
