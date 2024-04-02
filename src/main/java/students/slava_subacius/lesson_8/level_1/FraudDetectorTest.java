dc3ee9d30e0732b6c996d3a726c02628ccb21461
SlavaJava1 <150361254+SlavaJava1@users.noreply.github.com>
2024-04-02 08:32:26 +0300

    lesson_8_дополнение

--- src/main/java/students/slava_subacius/lesson_8/level_1/FraudDetectorTest.java
+++ src/main/java/students/slava_subacius/lesson_8/level_1/FraudDetectorTest.java
@@ -4,6 +4,7 @@ public class FraudDetectorTest {
     public static void main(String[] args) {
         testFraudulentTransaction();
         testNonFraudulentTransaction();
+        testLargeAmountTransaction();
     }
 
     public static void testFraudulentTransaction() {
@@ -19,17 +20,28 @@ public class FraudDetectorTest {
     }
 
     public static void testNonFraudulentTransaction() {
-
         Trader otherTrader = new Trader("John", "City");
         Transaction nonFraudulentTransaction = new Transaction(otherTrader, 1000);
 
         FraudDetector fraudDetector = new FraudDetector();
 
-
         if (!fraudDetector.isFraud(nonFraudulentTransaction)) {
             System.out.println("Test for non-fraudulent transaction passed.");
         } else {
             System.out.println("Test for non-fraudulent transaction failed.");
+        }
+    }
+
+    public static void testLargeAmountTransaction() {
+        Trader trader = new Trader("John", "City");
+        Transaction largeAmountTransaction = new Transaction(trader, 2000000);
+
+        FraudDetector fraudDetector = new FraudDetector();
+
+        if (fraudDetector.isFraud(largeAmountTransaction)) {
+            System.out.println("Test for large amount transaction passed.");
+        } else {
+            System.out.println("Test for large amount transaction failed.");
         }
     }
 }
