7cb07174aad33118ef283c529637fed19c023bdd
Antons Dolgopolovs <antdolg@gmail.com>
2024-04-04 18:00:29 +0300

    Add lesson_9 materials & homework

---
+++ src/main/java/teacher/lesson_9/homework/level_7_senior/task_32/Addition.java
@@ -0,0 +1,14 @@
+package teacher.lesson_9.homework.level_7_senior.task_32;//package teacher.lesson_9.homework.level_7_senior.task_32;
+//
+//class Addition extends TwoArgumentMathOperation {
+//
+//	public Addition(MathOperation leftArgument,
+//					MathOperation rightArgument) {
+//		super(leftArgument, rightArgument);
+//	}
+//
+//	@Override
+//	public double calculate() {
+//		return calculateLeftSide() + calculateRightSide();
+//	}
+//}
---
+++ src/main/java/teacher/lesson_9/homework/level_7_senior/task_32/Subtraction.java
@@ -0,0 +1,15 @@
+package teacher.lesson_9.homework.level_7_senior.task_32;//package teacher.lesson_9.homework.level_7_senior.task_32;
+//
+//class Subtraction extends TwoArgumentMathOperation {
+//
+//	public Subtraction(MathOperation leftArgument,
+//					   MathOperation rightArgument) {
+//		super(leftArgument, rightArgument);
+//	}
+//
+//	@Override
+//	public double calculate() {
+//		return calculateLeftSide() - calculateRightSide();
+//	}
+//}
+//
