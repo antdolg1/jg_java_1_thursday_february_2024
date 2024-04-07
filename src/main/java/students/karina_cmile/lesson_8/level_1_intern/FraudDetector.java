700cd68e6ad585adab3094b46e863d39240ffb27
KarinaSoha <karina.soha@gmail.com>
2024-04-07 20:07:23 +0300

    lesson 7, homework

---
+++ src/main/java/students/karina_cmile/lesson_8/level_1_intern/FraudDetector.java
@@ -0,0 +1,9 @@
+package students.karina_cmile.lesson_8.level_1_intern;
+
+public class FraudDetector {
+    boolean isFraud(Transaction t) {
+        return true;
+    }
+
+
+}
---
+++ src/main/java/students/karina_cmile/lesson_8/level_1_intern/Trader.java
@@ -0,0 +1,17 @@
+package students.karina_cmile.lesson_8.level_1_intern;
+
+public class Trader {
+    private String name;
+    private String city;
+
+    public Trader(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getCity() {
+        return this.city;
+    }
+}
