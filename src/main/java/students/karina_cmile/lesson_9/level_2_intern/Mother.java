1de9ea5f6c94a3b984d566571b0af71f394c8cf3
KarinaSoha <karina.soha@gmail.com>
2024-04-17 19:56:12 +0300

    lesson 9, homework

---
+++ src/main/java/students/karina_cmile/lesson_9/level_2_intern/Mother.java
@@ -0,0 +1,9 @@
+package students.karina_cmile.lesson_9.level_2_intern;
+
+public class Mother {
+    public static String parent1;
+    public Mother (String parent1){
+        Mother.parent1 ="Mother";
+    }
+}
+
