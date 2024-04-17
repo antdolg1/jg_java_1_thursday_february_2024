ed7d332676972ae205927989949cd3636ad33520
KarinaSoha <karina.soha@gmail.com>
2024-04-17 20:34:27 +0300

    lesson 9, homework

---
+++ src/main/java/students/karina_cmile/lesson_9/level_2_intern/Porsche911Demo.java
@@ -0,0 +1,11 @@
+package students.karina_cmile.lesson_9.level_2_intern;
+
+public class Porsche911Demo {
+    public static void main(String[] args) {
+
+        // продемонстрируйте создание инстанции класса Porsche911
+
+       Porsche911 porsche911 = new Porsche911(1967, 312);
+       System.out.println(porsche911);
+    }
+}
