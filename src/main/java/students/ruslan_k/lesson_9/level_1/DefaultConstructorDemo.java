82552bbf0063a2238217642f4aab73c68d12f236
KRuSS24 <russ0273@gmail.com>
2024-04-21 13:00:40 +0300

    Ruslan Lesson 9 lvl_1

--- src/main/java/students/ruslan_k/lesson_9/level_1/DefaultConstructorDemo.java
+++ src/main/java/students/ruslan_k/lesson_9/level_1/DefaultConstructorDemo.java
@@ -2,7 +2,7 @@ package students.ruslan_k.lesson_9.level_1;
 
 public class DefaultConstructorDemo {
     public static void main(String[] args) {
-        DefaultConstructor DC = new DefaultConstructor();
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
 
     }
 }
