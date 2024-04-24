750fa4706c99c7b48147768621dfd490904b932c
KarinaSoha <karina.soha@gmail.com>
2024-04-24 22:34:40 +0300

    lesson 10, homework

---
+++ src/main/java/students/karina_cmile/lesson_10/level_2_intern/Task_4/DayOfTheWeekDetectorIfVersion.java
@@ -0,0 +1,24 @@
+package students.karina_cmile.lesson_10.level_2_intern.Task_4;
+
+public class DayOfTheWeekDetectorIfVersion {
+    // Try to use switch here!!
+    public String detectDayName(int number) {
+        if (number == 1) {
+            return "Monday";
+        } else if (number == 2) {
+            return "Tuesday";
+        } else if (number == 3) {
+            return "Wednesday";
+        } else if (number == 4) {
+            return "Thursday";
+        } else if (number == 5) {
+            return "Friday";
+        } else if (number == 6) {
+            return "Saturday";
+        } else if (number == 7) {
+            return "Sunday";
+        } else {
+            return "Please input a valid number between 1 and 7";
+        }
+    }
+}
