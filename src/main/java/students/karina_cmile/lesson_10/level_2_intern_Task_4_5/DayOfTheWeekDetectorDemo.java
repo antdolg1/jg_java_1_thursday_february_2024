e10c338228975c51ac0b24ed305f1ddf0a69e76d
KarinaSoha <karina.soha@gmail.com>
2024-05-06 14:05:22 +0300

    lesson 10, homework

--- src/main/java/students/karina_cmile/lesson_10/level_2_intern_Task_4_5/DayOfTheWeekDetectorDemo.java
+++ src/main/java/students/karina_cmile/lesson_10/level_2_intern_Task_4_5/DayOfTheWeekDetectorDemo.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 
 public class DayOfTheWeekDetectorDemo {
 
-    private void run(){
-        System.out.print("Enter a number between 1 and 7: ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+    DayOfTheWeekDetector thisDay;
+    DayOfTheWeekDetectorDemo(DayOfTheWeekDetector thisDay) {
+        this.thisDay = thisDay;
     }
+        public void run () {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number between 1 and 7: ");
+            int number = scanner.nextInt();
+            String day = thisDay.detectDayName(number);
+            System.out.println("The " + number + "th day of the week calls " + day);
+        }
 
-
-
+    public static void main(String[] args) {
+        DayOfTheWeekDetectorDemo arrayVersion = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorSwitchVersion());
+        arrayVersion.run();
+        DayOfTheWeekDetectorDemo ifVersion = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorIfVersion());
+        ifVersion.run();
+        DayOfTheWeekDetectorDemo switchVersion = new DayOfTheWeekDetectorDemo(new DayOfTheWeekDetectorArrayVersion());
+        switchVersion.run();
+    }
 }
+
