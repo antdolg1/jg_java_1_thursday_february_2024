396606c96eee4bf02e9ff82ef46a5b86462706ae
KRuSS24 <russ0273@gmail.com>
2024-05-10 22:08:09 +0300

    Ruslan Lesson 10 lvl_3

---
+++ src/main/java/students/ruslan_k/lesson_10/level_3/FunctionalInterfaceExample.java
@@ -0,0 +1,8 @@
+package students.ruslan_k.lesson_10.level_3;
+
+//Interface with only one abstract method = Functional Interface
+@FunctionalInterface
+interface FunctionalInterfaceExample {
+    void abstractMethod();
+}
+
