dc3e399a9b840f66e14627d3229a9dee13b9fc33
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-05-10 15:22:50 +0300

    Homework_lesson_13_level_2_homework

---
+++ src/test/java/students/aleksandra_hodjuka/lesson_13/TaxCalculatorImplTest.java
@@ -0,0 +1,38 @@
+package students.aleksandra_hodjuka.lesson_13;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+ public class TaxCalculatorImplTest {
+     private TaxCalculatorImpl taxCalculator = new TaxCalculatorImpl();
+
+     @Test
+     public void shouldPayZeroTaxWhenIncomeIsZero() {
+         var income = BigDecimal.ZERO;
+         var taxToPay = taxCalculator.calculateTax(income);
+         assertEquals(taxToPay, new BigDecimal("0.00"));
+     }
+
+     @Test
+     public void shouldPay25PercentTaxWhenIncomeLessThen20000() {
+         var income = new BigDecimal(10000);
+         var taxToPay = taxCalculator.calculateTax(income);
+         assertEquals(taxToPay, new BigDecimal("2500.00"));
+     }
+
+     @Test
+     public void shouldPay25PercentTaxWhenIncomeEqualTo20000() {
+         var income = new BigDecimal(20000);
+         var taxToPay = taxCalculator.calculateTax(income);
+         assertEquals(taxToPay, new BigDecimal("5000.00"));
+     }
+     @Test
+     public void shouldPay40PercentTaxWhenIncomeBiggerThen20000() {
+         var income = new BigDecimal(30000);
+         var taxToPay = taxCalculator.calculateTax(income);
+         assertEquals(taxToPay, new BigDecimal("9000.00"));
+     }
+ }
+  
