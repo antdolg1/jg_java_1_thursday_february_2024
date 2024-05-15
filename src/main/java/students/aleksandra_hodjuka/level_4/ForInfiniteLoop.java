fef7e6b0d621f5603e3c33aa47b1a50c0c26ca84
Aleksandra_Hodjuka <alexandra11@inbox.lv>
2024-05-15 16:13:13 +0300

    Homework_lesson_6_and_level_3_homeworks

---
+++ src/main/java/students/aleksandra_hodjuka/level_4/ForInfiniteLoop.java
@@ -0,0 +1,15 @@
+package students.aleksandra_hodjuka.level_4;
+
+public class ForInfiniteLoop {
+    public static void main(String[] args) {
+        ForInfiniteLoop infiniteLoop = new ForInfiniteLoop();
+        infiniteLoop.runInfiniteLoop();
+    }
+
+    public void runInfiniteLoop() {
+        for (int i = 0; true; i++) {
+            System.out.println("Бесконечный цикл");
+        }
+    }
+
+}
