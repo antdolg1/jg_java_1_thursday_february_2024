f57ead7dc43d77c9d448f86033990c26bc8e205f
Antons Dolgopolovs <antdolg@gmail.com>
2024-05-16 21:45:00 +0300

    Add lesson_15 lessoncode

--- src/main/java/teacher/lesson_15/sample_code/Order.java
+++ src/main/java/teacher/lesson_15/sample_code/Order.java
@@ -1,36 +1,12 @@
 package teacher.lesson_15.sample_code;
 
+import lombok.*;
+
+@Data
+@Builder
 public class Order {
     private String id;
     private double totalAmount;
     private String user;
     private OrderStatus status;
-
-    public Order(String id, double totalAmount, String user, OrderStatus status) {
-        this.id = id;
-        this.totalAmount = totalAmount;
-        this.user = user;
-        this.status = status;
-    }
-
-    // Getters and setters
-    public String getId() {
-        return id;
-    }
-
-    public double getTotalAmount() {
-        return totalAmount;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
 }
--- src/main/java/teacher/lesson_15/sample_code/OrderProcessor.java
+++ src/main/java/teacher/lesson_15/sample_code/OrderProcessor.java
@@ -1,6 +1,15 @@
 package teacher.lesson_15.sample_code;
 
+/**
+ * This class contains method processOrder() and helper methods which contains logic for order processing.
+ * @author Vasja Pupkin
+ */
+
 public class OrderProcessor {
+    public static final String CANNOT_BE_PROCESSED_MESSAGE = "Order cannot be processed.";
+    public static final String SUCCESS_ORDER_MESSAGE = "Your order is being processed.";
+    public static final String FAILED_PAYMENT_MESSAGE = "Payment failed. Please retry.";
+
     private OrderDatabase db;
     private PaymentService paymentService;
     private NotificationService notificationService;
@@ -11,23 +20,59 @@ public class OrderProcessor {
         this.notificationService = notificationService;
     }
 
+    /**
+     * Process orders with status NEW to the business requirements mentioned in document ASSS21321
+     * @param orderId ID of the order
+     * @param userToken Unique userToken which identifies user in the system
+     */
     public void processOrder(String orderId, String userToken) {
-        Order order = db.getOrderById(orderId);
-        if (order != null && order.getStatus() == OrderStatus.NEW) {
-            boolean paymentConfirmed = paymentService.processPayment(order.getTotalAmount(), userToken);
-            if (paymentConfirmed) {
-                order.setStatus(OrderStatus.PROCESSING);
-                db.updateOrder(order);
-                notificationService.sendNotification(order.getUser(), "Your order is being processed.");
-            } else {
-                notificationService.sendNotification(order.getUser(), "Payment failed. Please retry.");
-            }
-        } else if (order != null && order.getStatus() != OrderStatus.NEW) {
-            notificationService.sendNotification(order.getUser(), "Order cannot be processed.");
+        Order order = getOrder(orderId);
+        if (order.getStatus() == OrderStatus.NEW) {
+            processNewOrder(order, userToken);
         } else {
-            throw new IllegalArgumentException("Invalid order ID");
+            notifyUser(order, CANNOT_BE_PROCESSED_MESSAGE);
         }
     }
+
+    private Order getOrder(String orderId) {
+        Order order = db.getOrderById(orderId);
+        if (order == null) {
+            throw new OrderNotFoundException(orderId);
+        }
+        return order;
+    }
+
+    private void processNewOrder(Order order, String userToken) {
+        if (paymentService.processPayment(order.getTotalAmount(), userToken)) {
+            updateOrderStatus(order);
+            notifyUser(order, SUCCESS_ORDER_MESSAGE);
+        } else {
+           notifyUser(order, FAILED_PAYMENT_MESSAGE);
+        }
+    }
+
+    private void updateOrderStatus(Order order) {
+        order.setStatus(OrderStatus.PROCESSING);
+        db.updateOrder(order);
+    }
+
+    private void notifyUser(Order order, String message) {
+        notificationService.sendNotification(order.getUser(), message);
+    }
+
+    //examples of creating Order with 'Builder' pattern
+    Order order1 = new Order("12", 12.0, "User", OrderStatus.NEW);
+
+    Order order2 = Order.builder()
+            .id("111")
+            .user("User_01")
+            .build();
+
+    Order order3 = Order.builder().build();
+
+    Order order4 = Order.builder()
+            .totalAmount(10.0)
+            .build();
 
     // Other methods...
 }
