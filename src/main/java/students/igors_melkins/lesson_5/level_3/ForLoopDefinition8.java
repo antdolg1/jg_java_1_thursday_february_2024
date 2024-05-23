1879a426e191ad802a03dfa99d76b788d123cd41
IgorsMelkins <Deco20@inbox.lv>
2024-05-23 23:24:09 +0300

    add homework lesson from lesson 5

---
+++ src/main/java/students/igors_melkins/lesson_5/level_3/ForLoopDefinition8.java
@@ -0,0 +1,10 @@
+package students.igors_melkins.lesson_5.level_3;
+
+public class ForLoopDefinition8 {
+    public static void main(String[] args) {
+        for ( int i=0 ;i< 101 ;i++ ) {
+            System.out.println(i);
+        }
+    }
+
+}
