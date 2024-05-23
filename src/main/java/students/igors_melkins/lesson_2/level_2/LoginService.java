6fdf5479e0d5c04f93f0c9cae53c86a333d5b21b
IgorsMelkins <Deco20@inbox.lv>
2024-05-23 10:35:58 +0300

    add homework lesson from lesson 2

---
+++ src/main/java/students/igors_melkins/lesson_2/level_2/LoginService.java
@@ -0,0 +1,7 @@
+package students.igors_melkins.lesson_2.level_2;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
