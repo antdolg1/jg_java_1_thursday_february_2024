0559b8c55a667af56a2e1ffa15fdabbdbf98cda6
IgorsMelkins <Deco20@inbox.lv>
2024-05-23 10:35:58 +0300

    add homework lesson from lesson 2

---
+++ src/main/java/students/igors_melkins/lesson_2/level_2/Phone.java
@@ -0,0 +1,7 @@
+package students.igors_melkins.lesson_2.level_2;
+
+public class Phone {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/students/igors_melkins/lesson_2/level_2/UserName.java
@@ -0,0 +1,12 @@
+package students.igors_melkins.lesson_2.level_2;
+
+import java.util.Scanner;
+
+public class UserName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter you username!");
+        String userName = scanner.next();
+        System.out.println("Hello," + userName + "!");
+    }
+}
