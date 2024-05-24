c98b942d540c8234df2e94d74ea21982f4ce3eea
KRuSS24 <russ0273@gmail.com>
2024-05-24 22:31:45 +0300

    Ruslan Lesson 11 lvl_2

---
+++ src/main/java/students/ruslan_k/lesson_11/level_2/Book.java
@@ -0,0 +1,43 @@
+package students.ruslan_k.lesson_11.level_2;
+
+import java.util.Objects;
+
+class Book {
+    private Long id;
+    private String title;
+    private String author;
+
+    Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author);
+    }
+}
