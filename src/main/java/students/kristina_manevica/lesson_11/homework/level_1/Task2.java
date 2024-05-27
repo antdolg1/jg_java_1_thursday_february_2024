49cf96a0a648be5d14e940adb4d318e8f73539be
MANKRA <kristina.manevica@if.lv>
2024-05-27 21:40:02 +0300

    Homework for lesson 11

---
+++ src/main/java/students/kristina_manevica/lesson_11/homework/level_1/Task2.java
@@ -0,0 +1,32 @@
+package students.kristina_manevica.lesson_11.homework.level_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println("List Integer elements:");
+        for (Integer number : numbers) {
+            System.out.println(number);
+        }
+
+
+        List<String> names = new LinkedList<>();
+        names.add("One");
+        names.add("Two");
+        names.add("Three");
+        names.set(0, "Zero");
+
+        System.out.println("List String elements:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
