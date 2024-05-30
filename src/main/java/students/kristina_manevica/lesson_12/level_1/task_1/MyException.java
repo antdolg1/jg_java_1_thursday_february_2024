8b5a831b4f026551561fe7269b14451209b64f2a
MANKRA <kristina.manevica@if.lv>
2024-05-30 21:10:59 +0300

    Homework for lesson 12

---
+++ src/main/java/students/kristina_manevica/lesson_12/level_1/task_1/MyException.java
@@ -0,0 +1,5 @@
+package students.kristina_manevica.lesson_12.level_1.task_1;
+
+public class MyException extends Throwable {
+
+}
---
+++ src/main/java/students/kristina_manevica/lesson_12/level_1/task_3/MyException.java
@@ -0,0 +1,5 @@
+package students.kristina_manevica.lesson_12.level_1.task_3;
+
+public class MyException extends Exception {
+
+}
