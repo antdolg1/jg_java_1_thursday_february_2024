77212e3043efe94c9ccbfa6adebd75b5bc4b17cb
MANKRAA <tuc_tuc@inbox.lv>
2024-06-03 20:02:09 +0300

    Homework for lesson 13

--- src/main/java/students/kristina_manevica/lesson_13/homework/level_1/TaxCalculatorImpl.java
+++ src/main/java/students/kristina_manevica/lesson_13/homework/level_1/TaxCalculatorImpl.java
@@ -1,15 +1,29 @@
 package students.kristina_manevica.lesson_13.homework.level_1;
 
 import java.math.BigDecimal;
-//import java.math.RoundingMode;
+import java.math.RoundingMode;
 
 public class TaxCalculatorImpl implements TaxCalculator {
+
+    BigDecimal taxRate25 = new BigDecimal("0.25");
+    BigDecimal taxRate40 = new BigDecimal("0.40");
+
     @Override
     public BigDecimal calculateTax(BigDecimal income) throws IllegalArgumentException {
-        BigDecimal taxRate = new BigDecimal("0.25");
+
         if (income.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("No negative tax rates!");
+            throw new IllegalArgumentException("Negative income not allowed!");
         }
-        return income.multiply(taxRate);//.setScale(2, RoundingMode.HALF_UP);
+
+        if (income.compareTo(new BigDecimal("20000")) <= 0) {
+            return income.multiply(taxRate25).setScale(2, RoundingMode.HALF_UP);
+        } else {
+            BigDecimal incomeMoreThen20000 = income.subtract(new BigDecimal("20000"));
+            BigDecimal taxAfter20000 = incomeMoreThen20000.multiply(taxRate40).setScale(2,
+                    RoundingMode.HALF_UP);
+            BigDecimal taxBefore20000 = new BigDecimal("20000").multiply(taxRate25).setScale(2,
+                    RoundingMode.HALF_UP);
+            return taxBefore20000.add(taxAfter20000);
+        }
     }
 }
