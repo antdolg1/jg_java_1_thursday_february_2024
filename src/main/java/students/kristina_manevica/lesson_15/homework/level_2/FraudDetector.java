557013492b1a16cd9272ed8344a9c6795d1f1c86
MANKRA <kristina.manevica@if.lv>
2024-06-14 14:38:32 +0300

    Homework for lesson 15

--- src/main/java/students/kristina_manevica/lesson_15/homework/level_2/FraudDetector.java
+++ src/main/java/students/kristina_manevica/lesson_15/homework/level_2/FraudDetector.java
@@ -3,16 +3,19 @@ package students.kristina_manevica.lesson_15.homework.level_2;
 public class FraudDetector {
 
     public boolean isFraud(Trader trader) {
+        if (trader == null) {
+            throw new IllegalArgumentException("Trader cannot be null!");
+        }
         return isFraudulentTraderName(trader) || isFraudulentCity(trader);
     }
 
     private boolean isFraudulentTraderName(Trader trader) {
         String traderName = trader.getFullName();
-        return traderName.equals("Pokemon");
+        return traderName != null && traderName.equals("Pokemon");
     }
 
     private boolean isFraudulentCity(Trader trader) {
         String cityName = trader.getCity();
-        return cityName.equals("Sidney");
+        return cityName != null && cityName.equals("Sidney");
     }
 }
--- src/test/java/students/kristina_manevica/lesson_15/homework/level_2/FraudDetectorTest.java
+++ src/test/java/students/kristina_manevica/lesson_15/homework/level_2/FraudDetectorTest.java
@@ -1,11 +1,45 @@
 package students.kristina_manevica.lesson_15.homework.level_2;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class FraudDetectorTest {
 
+    private static FraudDetector fraudDetector;
+
+    @BeforeAll
+    public static void setUp() {
+        fraudDetector = new FraudDetector();
+    }
+
     @Test
-    public void testIsFraud() {
+    public void testFraudTrader_ByName() {
+        Trader trader = new Trader("Riga", "Pokemon");
+        Assertions.assertTrue(fraudDetector.isFraud(trader), "Should be detected as fraud due to name!");
+    }
+
+    @Test
+    public void testFraudTrader_ByCity() {
+        Trader trader = new Trader("Sidney", "Baltazar");
+        Assertions.assertTrue(fraudDetector.isFraud(trader), "Should be detected as fraud due to city!");
+    }
+
+    @Test
+    public void testFraudTrader_ByName_And_City() {
+        Trader trader = new Trader("Sidney", "Pokemon");
+        Assertions.assertTrue(fraudDetector.isFraud(trader), "Should be detected as fraud due to name and city!");
+    }
+
+    @Test
+    public void testTrader_With_NoFraudIndications() {
+        Trader trader = new Trader("Marupe", "Ezhik");
+        Assertions.assertFalse(fraudDetector.isFraud(trader), "Should not be detected as fraud, no indications!");
+    }
+
+    @Test
+    public void test_NullTrader_ThrowsException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> fraudDetector.isFraud(null),
+                "Method isFraud should throw IllegalArgumentException when Trader is null!");
     }
 }
