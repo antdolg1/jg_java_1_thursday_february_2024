cfb1f8fef6b13059682aad5fe653df54ab1ae5f7
tnejq <tnejq2012@gmail.com>
2024-06-20 19:41:28 +0300

    Домашка за 13-ый урок.

---
+++ src/main/java/students/arturs_novikovs/lesson_13/TaxCalculator.java
@@ -0,0 +1,6 @@
+package students.arturs_novikovs.lesson_13;
+
+import java.math.BigDecimal;
+public interface TaxCalculator {
+    BigDecimal calculateTax(BigDecimal income);
+}
---
+++ src/test/java/students/arturs_novikovs/lesson_13/TaxCalculatorImplTest.java
@@ -0,0 +1,51 @@
+package students.arturs_novikovs.lesson_13;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+
+class TaxCalculatorImplTest {
+    private static TaxCalculatorImpl taxCalculator;
+
+    @BeforeAll
+    static void setUp(){
+    taxCalculator = new TaxCalculatorImpl();
+    }
+    @Test
+    void calculateTax_WhenIncome_Negative() {
+        BigDecimal income = BigDecimal.valueOf(-1.00);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            taxCalculator.calculateTax(income);
+        });
+    }
+
+    @Test
+    void calculateTax_WhenIncome_Zero() {
+        BigDecimal income = BigDecimal.ZERO;
+        BigDecimal actual = taxCalculator.calculateTax(income);
+        Assertions.assertEquals(new BigDecimal("0.00"), actual);
+    }
+
+    @Test
+    void calculateTax_WhenIncome_LessThen20K() {
+        BigDecimal income = new BigDecimal("19999.99");
+        BigDecimal actual = taxCalculator.calculateTax(income);
+        Assertions.assertEquals(new BigDecimal("5000.00"), actual);
+    }
+
+    @Test
+    void calculateTax_WhenIncome_20K() {
+        BigDecimal income = new BigDecimal("20000.00");
+        BigDecimal actual = taxCalculator.calculateTax(income);
+        Assertions.assertEquals(new BigDecimal("5000.00"), actual);
+    }
+
+    @Test
+    public void calculatePay40PercentTax_WhenIncome_MoreThen20000() {
+        BigDecimal income = new BigDecimal("20000.10");
+        BigDecimal actual = taxCalculator.calculateTax(income);
+        Assertions.assertEquals(new BigDecimal("5000.04"), actual);
+    }
+}
